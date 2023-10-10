d718a5adbe5b08aa64f098c9c6c94b04cb98cf44
badiskefi <badiskefi@gmail.com>
2023-10-10 19:09:38 +0200

    adding dynamic behavior to image profile and name

--- lib/data.ts
+++ lib/data.ts
@@ -4,6 +4,7 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
+import meImg from "@/public/me.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -83,7 +84,10 @@ export const projectsData = [
     imageUrl: wordanalyticsImg,
   },
 ] as const;
-
+export const MeData = {
+  name: "Badis Kefi",
+  imageUrl: meImg,
+}
 export const skillsData = [
   "HTML",
   "CSS",
